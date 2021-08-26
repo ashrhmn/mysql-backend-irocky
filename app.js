f223f52acc54e4d0c66090852dc8b25ea43db6c9
Ashik Rahman <ashrhmn99@gmail.com>
2021-08-27 02:17:57 +0600

    Add getAll and createNew routes

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+
 const routes = require('./routes')
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.use('/api/', routes)
 
-app.listen(5000, () => console.log(`http://localhost:5000`))
+app.listen(5000, () => console.log(`http://localhost:5000/api`))
--- routes.js
+++ routes.js
@@ -2,4 +2,19 @@ const router = require('express').Router()
 
 const connection = require('./database')
 
+router.get('/', (req, res) => {
+    connection.query(`SELECT id,name from book`, (err, results, field) => {
+        if (err) return res.send(err)
+        return res.json(results)
+    })
+})
+
+router.post('/', (req, res) => {
+    const book = req.body.name
+    connection.execute(`insert into book(name) values('${book}')`, (err, result) => {
+        if (err) return res.send(err)
+        return res.json(result)
+    })
+})
+
 module.exports = router
