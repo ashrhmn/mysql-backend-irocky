e150122812b0ae5acb9c8cb03617216bf6866d25
Ashik Rahman <ashrhmn99@gmail.com>
2021-08-27 02:20:38 +0600

    Add update routes

--- routes.js
+++ routes.js
@@ -17,4 +17,13 @@ router.post('/', (req, res) => {
     })
 })
 
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    const book = req.body.name
+    connection.execute(`replace into book(id,name) values(${id},'${book}')`, (err, result) => {
+        if (err) return res.send(err)
+        return res.json(result)
+    })
+})
+
 module.exports = router
